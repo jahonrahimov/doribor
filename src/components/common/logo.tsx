f50f73af57fb87ab1180832e9bc4ae8a6b5baf27
Jahon <rahimovjahongir951@gmail.com>
2023-06-22 14:43:35 +0500

    fix: add next link

--- src/components/common/logo.tsx
+++ src/components/common/logo.tsx
@@ -1,12 +1,14 @@
+import Link from "next/link";
+
 const Logo = () => (
-  <a className="mr-4" href="https://flowbite.com/">
+  <Link href="/" className="mr-4">
     <span className="shadow self-center md:text-xl text-[14px] md:py-2 py-1.5 pl-4 pr-1 rounded-l-3xl bg-green-500 text-white font-bold whitespace-nowrap dark:text-white">
       DORI
     </span>
     <span className="shadow self-center md:text-xl text-[14px] md:py-2 py-1.5 pr-4 pl-1 rounded-r-3xl bg-orange-500 text-white font-bold whitespace-nowrap dark:text-white">
       BOR
     </span>
-  </a>
+  </Link>
 );
 
 export default Logo;
