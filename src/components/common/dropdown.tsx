249d7cc4b69eedbf7374306ed9a25fe42b064f07
Jahon <rahimovjahongir951@gmail.com>
2023-06-23 06:30:30 +0500

    fix: remove padding from dropdown

--- src/components/common/dropdown.tsx
+++ src/components/common/dropdown.tsx
@@ -34,9 +34,9 @@ const Dropdown: FC<DropdownProps> = (props) => {
             leaveFrom="transform opacity-100 scale-100"
             leaveTo="transform opacity-0 scale-95"
           >
-            <Menu.Items className="absolute right-0 mt-2 w-[240px] origin-top-right divide-y divide-gray-100 rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+            <Menu.Items className="absolute right-0 mt-4 w-[240px] origin-top-right divide-y divide-gray-100 rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
               {options.map((item, i) => (
-                <div key={i} className="px-1 py-1 ">
+                <div key={i}>
                   <Menu.Item>
                     {({ active }) => (
                       <div className="hover:bg-white w-full text-[14px] px-[14.5px] group flex text-sm rounded-lg h-[40px] items-center cursor-pointer">
